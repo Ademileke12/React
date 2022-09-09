5419d8769f802fc80cf6edb185ea0dfc065c6dcf
ademilekeabudu@gmail.com <ademilekeabudu@gmail.com>
2022-09-09 08:35:53 +0100

    React

--- ReactLearning/react_02_nodejs/my-app/src/App.js
+++ ReactLearning/react_02_nodejs/my-app/src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends React.Component {
+	constructor(){
+		super();
+			
+			this.state =
+			{
+				name: "James",
+				surname: "jason"
+			}
+			
+			this.handleChange = (e) => {
+				this.setState({
+					name:e.target.value
+				})
+			}
+	}
+	render(){
+	  return(
+	   <div className="App">
+         <h1>My first name is {this.state.name}</h1>
+		  <form>
+		   <input type ="text" placeholder ="entr name" onChange={this.handleChange}/>
+		    <button>submit</button>
+		  </form>
+       </div>
+	  )
+	}
 }
 
 export default App;
