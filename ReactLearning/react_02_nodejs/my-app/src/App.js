fbfab42554bcdfc62c79bfc3641e00d520023292
Ademileke <38130432+Ademileke12@users.noreply.github.com>
2022-09-09 16:52:50 +0100

    added work

--- ReactLearning/react_02_nodejs/my-app/src/App.js
+++ ReactLearning/react_02_nodejs/my-app/src/App.js
@@ -7,23 +7,43 @@ class App extends React.Component {
 			
 			this.state =
 			{
-				name: "James",
-				surname: "jason"
+				name: "Liam",
+				skill: "React"
 			}
-			
+
 			this.handleChange = (e) => {
 				this.setState({
-					name:e.target.value
+					name: e.target.value
 				})
+			}
+
+			this.handleSubmit = (e) => {
+				e.preventDefualt();
+				console.log('This form was submitted by', this.state.name)
+			}
+
+			this.handleClick = (e) => {
+				console.log('This button was clicked')
+			}
+
+			this.handleMouseover = (e) => {
+				console.log('stop that tickels')
+			}
+
+			this.handleCopy = (e) => {
+				console.log('stop stealing my content')
 			}
 	}
 	render(){
 	  return(
 	   <div className="App">
          <h1>My first name is {this.state.name}</h1>
-		  <form>
-		   <input type ="text" placeholder ="entr name" onChange={this.handleChange}/>
+		  <form onSubmit={this.handleSubmit}>
+		   <input type ="text" placeholder ="enter name" onChange={this.handleChange} />
 		    <button>submit</button>
+			<button onClick={this.handleClick}>click me</button>
+			<button onMouseOver={this.handleMouseover}>Hover over me</button>
+			<p onCopy={this.handleCopy}>My first react app test</p>
 		  </form>
        </div>
 	  )
